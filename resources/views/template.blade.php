05d2cbc0cc1a561f24fea7e724dcc34acb4b2274
RGDayan <tristan.reiss@hotmail.com>
2022-02-27 19:39:21 +0100

    Presentation de la page Accueil

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -19,12 +19,12 @@
             overflow-x-hidden">
 
         <!-- Javascript -->
-	    <script src="{{ asset('js/app.js') }}"></script>
+	    {{-- <script src="{{ asset('js/app.js') }}"></script> --}}
 
         <!-- Header -->
         <header 
             class="flex flex-col
-                w-full md:h-116
+                w-full lg:h-116
                 items-center justify-center
                 overflow-hidden
                 -z-10"
@@ -48,27 +48,102 @@
                 text-white
                 text-base
                 font-normal
+                md:flex-row
                 phone:font-semibold
                 phone:text-xl">
 
                 <!-- Logo -->
                 <img class="w-16
-                    shadow-4xl-center 
-                    shadow-orange-300 
                     rounded-full
+                    shadow-4xl-center shadow-orange-300 
+                    anti
                     phone:w-44"
                     src="{{ asset('storage/image/Logo_Dayan.png') }}"/>
                 
-                <!-- Texte -->
-                <h1>TRISTAN REISS</h1>
-                <h2>Technicien en Programmation Informatique</h2>
-            
+                <div class="ml-10">
+                    <!-- Texte -->
+                    <h1 class="font-mukta text-6xl ">
+                        Tristan Reiss
+                    </h1>
+                    <h2 class="font-mukta text-2xl">Technicien en Programmation Informatique</h2>
+                </div>
             </div>
             
         </header>
 
         <!-- Corps de la page -->
-        <main class="">
+        <main class="flex flex-col
+            w-full
+            items-center">
+
+            <!-- Présentation -->
+            <div class="flex flex-col
+                w-full 
+                items-center justify-between
+                p-10
+                lg:flex-row">
+
+                <!-- Photo de profil -->
+                <img class="max-w-md 
+                    rounded-xl"
+                     src="{{ asset('storage/image/photo_profil.jpg') }}">
+
+                <div class="w-full
+                    -mt-10 p-10
+                    bg-gradient-to-br from-yellow-500 to-orange-600 
+                    grayscale-25 opacity-75
+                    md:-ml-10">
+
+                    <!-- Titre présentation -->
+                    <h1 class="w-full
+                        text-center text-white
+                        text-2xl font-semibold">
+                        Alternant en Développement Informatique
+                    </h1>
+
+                    <!-- Description -->
+                    <p class="mt-10 
+                        text-center text-white text-lg font-medium">
+                        Développeur web et logiciel, graphiste amateur,
+                        streamer à mes temps perdus et créateur de contenu.
+                        <br>
+                        Découvrez mon profil, mes projets et mon parcours sur ce Portfolio.
+                    </p>
+                    
+                </div>
+            </div>
+
+            
+            <div class="flex flex-col
+                w-full 
+                items-center justify-between
+                p-10
+                lg:flex-row">
+                
+                <div class="w-full p-10
+                    bg-gradient-to-br from-yellow-500 to-orange-600 
+                    grayscale-25 opacity-75">
+
+                    <h1 class="w-full
+                        text-center text-white
+                        text-2xl font-semibold">
+                        Alternant en Développement Informatique
+                    </h1>
+
+                    <p class="mt-10 
+                        text-center text-white text-lg font-medium">
+                        Développeur web et logiciel, graphiste amateur,
+                        streamer à mes temps perdus et créateur de contenu.
+                        <br>
+                        Découvrez mon profil, mes projets et mon parcours sur ce Portfolio.
+                    </p>
+                    
+                </div>
+
+                <img class="max-w-md 
+                    rounded-xl"
+                     src="{{ asset('storage/image/photo_profil.jpg') }}">
+            </div>
             
         </main>
     </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,21 @@ module.exports = {
   content: [],
   theme: {
     extend: {
+      screens: {
+        'phone' : '521px',
+      },
       height: {
         '116': "30rem",
       },
-      screens: {
-        'phone' : '521px',
+      borderRadius:{
+        "fancy-1": "9% 91% 8% 92% / 74% 20% 80% 26%  ",
       },
       boxShadow: {
         '4xl-center' : '0 0 40px 10px rgba(0, 0, 0, 0.3)',
       },
       grayscale: {
+        12: '12,5%',
+        25: '25%',
         50: '50%',
       },
     },
